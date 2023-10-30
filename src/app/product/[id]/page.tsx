01398462946cedfbef72d60a8a868c8c9ab77738
Nurlan <iamsamurai1991@gmail.com>
2023-10-30 19:50:24 +0100

    modified:   src/app/product/[id]/page.tsx

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const SingleProductPage = () => {
   return (
-    <div>
+    <div className=" h-screen">
       <div className="relative">
         <Image
           objectFit="contain"
