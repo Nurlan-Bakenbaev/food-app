9a76f9fb3c28448b7ce678aa7b7a327458280104
Nurlan <iamsamurai1991@gmail.com>
2023-10-30 19:47:54 +0100

    modified:   src/app/product/[id]/page.tsx modified:   src/app/product/page.tsx

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
 import React from "react";
 
 const SingleProductPage = () => {
-  return <div>SingleProductPage</div>;
+  return (
+    <div>
+      <div className="relative">
+        <Image
+          objectFit="contain"
+          layout="fill"
+          alt="Product-Image"
+          src={"/p1.png"}
+        />
+      </div>
+      <div>
+        <h1 className="text-4xl">Sicilian</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+          iusto provident laboriosam, maxime dignissimos fuga alias hic
+          quibusdam soluta harum, beatae optio delectus sed mollitia quidem
+          nostrum. Sunt, obcaecati sequi.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SingleProductPage;
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const ProductsPage = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <div>
+        <Image alt="Product-Image" src={"/p1.png"} />
+      </div>
+      <div>
+        <h1 className="text-4xl">Sicilian</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+          iusto provident laboriosam, maxime dignissimos fuga alias hic
+          quibusdam soluta harum, beatae optio delectus sed mollitia quidem
+          nostrum. Sunt, obcaecati sequi.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
